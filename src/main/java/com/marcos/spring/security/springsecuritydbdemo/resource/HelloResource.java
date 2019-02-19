b3a656ab50962f4dd197c9ec20c4b8beeb39d435
Marcos Gui <marcos.gui@spsoft-cn.com>
2019-02-19 18:10:18 +0800

    [ADD] Add home page

--- src/main/java/com/marcos/spring/security/springsecuritydbdemo/resource/HelloResource.java
+++ src/main/java/com/marcos/spring/security/springsecuritydbdemo/resource/HelloResource.java
@@ -2,6 +2,7 @@ package com.marcos.spring.security.springsecuritydbdemo.resource;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class HelloResource{
     return "Hello Welcome";
   }
 
-//  @PreAuthorize("hasAnyAuthority('ADMIN')")
+  @PreAuthorize("hasAnyAuthority('ADMIN')")
   @GetMapping("/secured")
   public String securedHello() {
     return "Secured Hello";
